e376e36fad52c700149427a9914e932f916ffc0a
David Keller <dckeller1@gmail.com>
2017-12-01 20:31:39 -0700

    Add in previous modules

--- stack.rb
+++ stack.rb
@@ -1,3 +1,24 @@
 class Stack
 
+	def initialize
+		@array = Array.new
+	end
+
+	def push(element)
+		@array.push(element)
+	end
+
+	def pop
+		@array.pop
+		@array
+	end
+
+	def top
+		@array.last
+	end
+
+	def empty?
+		@array.empty?
+	end  
+
 end
